e9561e4b74cd8d0ddb66f332d4a58d21d7428ace
Julia Oldman <78836093+JuliaOldman0@users.noreply.github.com>
2025-03-10 22:31:12 -0400

    saving my work on project 10

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import "./Header.css";
+import logo from "../../assets/logo.svg";
+import avatar from "../../assets/avatar.png";
+
+function Header() {
+  return <header className="header">
+    <img className="header__logo" src={logo} alt="Logo" />
+    <p className="header__date-and-location">Date, Location</p>
+    <button className="header__add-clothes-btn">+ Add clothes</button>
+    <div className="header__user-container">
+      <p className="header__user-name">Terrence Tegegne</p>
+      <img className="header__user-avatar" src={avatar} alt="Terrence Tegegne" />
+    </div>
+  </header>;
+}
+
+export default Header;
