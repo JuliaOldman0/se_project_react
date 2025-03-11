7a42c9665c8560b6d86489a8de975b203a187df8
Julia Oldman <78836093+JuliaOldman0@users.noreply.github.com>
2025-03-11 12:27:54 -0400

    saving work

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,11 +2,11 @@ import "./Header.css";
 import logo from "../../assets/logo.svg";
 import avatar from "../../assets/avatar.png";
 
-function Header() {
+function Header({handleAddClick}) {
   return <header className="header">
     <img className="header__logo" src={logo} alt="Logo" />
     <p className="header__date-and-location">Date, Location</p>
-    <button className="header__add-clothes-btn">+ Add clothes</button>
+    <button onClick={handleAddClick} type="button" className="header__add-clothes-btn">+ Add clothes</button>
     <div className="header__user-container">
       <p className="header__user-name">Terrence Tegegne</p>
       <img className="header__user-avatar" src={avatar} alt="Terrence Tegegne" />
