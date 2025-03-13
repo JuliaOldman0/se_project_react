01b66480165879fb1e54db953b8540461f3a627b
Julia Oldman <78836093+JuliaOldman0@users.noreply.github.com>
2025-03-12 21:54:40 -0400

    first submission project 10

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import "./Footer.css";
+
+function Footer() {
+    return (
+      <div className="footer">
+        <p className="footer__title">Developed by Julia Oldman</p>
+        <p className="footer__year">2025</p>
+      </div>
+    );
+  }
+  
+  export default Footer;
+  
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,16 +2,35 @@ import "./Header.css";
 import logo from "../../assets/logo.svg";
 import avatar from "../../assets/avatar.png";
 
-function Header({handleAddClick}) {
-  return <header className="header">
-    <img className="header__logo" src={logo} alt="Logo" />
-    <p className="header__date-and-location">Date, Location</p>
-    <button onClick={handleAddClick} type="button" className="header__add-clothes-btn">+ Add clothes</button>
-    <div className="header__user-container">
-      <p className="header__user-name">Terrence Tegegne</p>
-      <img className="header__user-avatar" src={avatar} alt="Terrence Tegegne" />
-    </div>
-  </header>;
+function Header({ handleAddClick, weatherData }) {
+  const currentDate = new Date().toLocaleString("default", {
+    month: "long",
+    day: "numeric",
+  });
+
+  return (
+    <header className="header">
+      <img className="header__logo" src={logo} alt="Logo" />
+      <p className="header__date-and-location">
+        {currentDate}, {weatherData.city}
+      </p>
+      <button
+        onClick={handleAddClick}
+        type="button"
+        className="header__add-clothes-btn"
+      >
+        + Add clothes
+      </button>
+      <div className="header__user-container">
+        <p className="header__user-name">Terrence Tegegne</p>
+        <img
+          className="header__user-avatar"
+          src={avatar}
+          alt="Terrence Tegegne"
+        />
+      </div>
+    </header>
+  );
 }
 
 export default Header;
